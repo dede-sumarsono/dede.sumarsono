c91af5e74541b2ba0e1a45fa09bcc3e3b9d8d72d
dede-sumarsono <57135161+dede-sumarsono@users.noreply.github.com>
2019-10-29 20:33:52 +0700

    Add files via upload
    Tugas Continue/break

---
+++ DedeSumarsono_continuebreak/src/dedesumarsono_continuebreak/DedeSumarsono_continuebreak.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dedesumarsono_continuebreak;
+
+/**
+ *
+ * @author DD
+ */
+public class DedeSumarsono_continuebreak {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        
+        // Statement 4
+        int i =0;
+        while (i<10){
+            
+                if(i==4){
+                    i++;
+                    continue;
+                }
+                System.out.println(i);
+                i++;
+        }
+        
+        /* Statement 3
+        int i =0;
+        while (i<10){
+            System.out.println(i);
+            i++;
+            
+                if(i==4){
+                    break;
+                }
+        } */
+        
+        /*
+        
+        Statement 2
+        
+        for (int i =0; i<10;i++){
+        
+            if(i==4){
+             continue;
+            }
+            System.out.println(i);
+            
+            
+            /*Statement 1
+            
+            if(i==4){
+             break;
+            }
+            System.out.println(i);}*/ 
+        
+        
+    }
+    
+}
